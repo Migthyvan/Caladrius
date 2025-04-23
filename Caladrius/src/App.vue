1c589dc45bfc15cee6597fd5af4011f7644b93f8
Yvan <anguiyvan@gmail.com>
2025-04-23 23:11:50 +0200

    first commit

---
+++ Caladrius/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <router-view /> <!-- Les routes s'affichent ici -->
+</template>
---
+++ Caladrius/src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>Bienvenue au pays mon fils</h1>
+        <p>c'est la page principale de Caladrius</p>
+        <router-link to="/contact">Contactez nous !</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
