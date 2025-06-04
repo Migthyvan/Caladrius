3e5c3032dc6b86daaed56d4078d970d6bb33fc0d
miakojean <miakojeanyves@gmail.com>
2025-06-04 10:41:00 +0000

    routes netfily

--- Caladrius/src/router/index.js
+++ Caladrius/src/router/index.js
@@ -7,46 +7,112 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  
   {
     path: '/about',
     name: 'About',
     component: () => import('../views/about.vue')
   },
   {
+    path:'/external-about',
+    name: 'external-about',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/about'
+    }
+  },
+  
+  {
     path:'/services',
     name: 'services',
     component: () => import('../views/services.vue')
   },
+  {
+    path:'/external-services',
+    name: 'external-services',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/services'
+    }
+  },
+  
   {
     path:'/contact',
     name: 'contact',
     component: () => import('../views/contact.vue')
   },
   {
+    path:'/external-contact',
+    name: 'external-contact',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/contact'
+    }
+  },
+  
+  {
     path:'/newsletter',
     name: 'newsletter',
     component: () => import('../views/newsletter.vue')
   },
+  {
+    path:'/external-newsletter',
+    name: 'external-newsletter',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/newsletter'
+    }
+  },
+
   {
     path:'/conditions',
     name: 'conditions',
     component: () => import('../components/policies/conditions.vue')
   },
   {
+    path:'/external-conditions',
+    name: 'external-conditions',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/conditions'
+    }
+  },
+
+  {
     path:'/confidentialité',
     name: 'confidentialité',
     component: () => import('../components/policies/confidentialité.vue')
   },
+  {
+    path:'/external-confidentialité',
+    name: 'external-confidentialité',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/confidentialité'
+    }
+  },
+
   {
     path:'/valeurs_morales_et_éthique',
     name: 'valeurs_morales_et_éthique',
     component: () => import('../components/policies/valeurs_morales_et_éthique.vue')
   },
   {
+    path:'/external-valeurs_morales_et_éthique',
+    name: 'external-valeurs_morales_et_éthique',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/valeurs_morales_et_éthique'
+    }
+  },
+
+
+  {
     path:'/mentions_de_copyright',
     name: 'mentions_de_copyright',
     component: () => import('../components/policies/mentions_de_copyright.vue')
   },
+  {
+    path:'/external-mentions_de_copyright',
+    name: 'external-mentions_de_copyright',
+    beforeEnter() {
+      window.location.href = 'https://caladriustech.com/mentions_de_copyright'
+    }
+  },
+
   {
     path:'/droits_auteurs',
     name: 'droits_auteurs',
